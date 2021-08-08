d3772cb7354a1194e23f11deace24e2705898492
freddymanston <freddymanston@gmail.com>
2021-08-08 15:00:52 +0200

    initial commit

---
+++ src/main/java/com/fmanston/springchatapp/config/WebSocketConfig.java
@@ -0,0 +1,38 @@
+package com.fmanston.springchatapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
+{
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry)
+    {
+        System.out.println("registerStompEndpoints");
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry)
+    {
+        System.out.println("configureMessageBroker");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
+
+
+        //   Use this for enabling a Full featured broker like RabbitMQ
+
+        /*
+        registry.enableStompBrokerRelay("/topic")
+                .setRelayHost("localhost")
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest");
+        */
+    }
+}
---
+++ src/test/java/com/fmanston/springchatapp/SpringChatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmanston.springchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
